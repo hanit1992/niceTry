9a48a7d3e746e7af87696cb59ce34a771ec97746
hanit1992 <hanit.hakim@mail.huji.ac.il>
2018-03-24 10:08:59 +0300

    nnn+++++99

--- src/Builder/Main.java
+++ src/Builder/Main.java
@@ -3,6 +3,6 @@ package Builder;
 public class Main {
     public static void main(String[] args){
         Sendi s = new Sendi.Builder(4,5).age(12).buildSendi();
-        System.out.println(s+"hh"+"mfmfmfmf");
+        System.out.println(s+"hh"+"mfmfmfmf"+"sss");
     }
 }
